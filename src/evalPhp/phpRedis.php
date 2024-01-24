06daa5ed762fd9cbde3331aa66f93d4f0c5b405a
LimChangMin <cml039465@gmail.com>
2024-01-24 14:45:46 +0900

    .

--- src/evalPhp/phpRedis.php
+++ src/evalPhp/phpRedis.php
@@ -44,7 +44,7 @@ if ($SERV_ENV == "TRUE") {
     $sessionHandler = new Predis\Session\Handler($redis);
     session_set_save_handler($sessionHandler);
 } 
-
+// session_cache_expire(720); // 기존에 사용하던 코드
 ini_set('session.gc_maxlifetime', 43200); // 세션유지시간(s) 12시간
 
 /* 세션 생성 및 선언 */
