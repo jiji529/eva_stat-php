3c4c29772cab2e1dd92c217ad4a35ee325fd1d02
LimChangMin <cml039465@gmail.com>
2024-01-29 10:22:32 +0900

    .

--- src/evalPhp/newTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$tokenUrl = "https://web-viewer.scrapmaster.co.kr/admin/adminLoginTokenMake.do?smId=".$uid;
-	$tokenRet = file_get_contents($tokenUrl, false, $ctx);
-
-	var_dump($tokenRet);
-?>
